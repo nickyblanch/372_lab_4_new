a0ab7409d2c4c94c5b0fcab0c227a433bab31faf
nickyblanch <70228627+nickyblanch@users.noreply.github.com>
2021-10-26 10:57:17 -0700

    Update switch.cpp

--- src/switch.cpp
+++ src/switch.cpp
@@ -14,14 +14,14 @@
  */
 void initSwitchPD0(){
 
-    // Initialize PD0 (pin 50) for input (set DDR to input). Enable pullup resistor.
+    // Initialize PD0 (pin 21) for input (set DDR to input). Enable pullup resistor.
     // NOTE: To set the port to input, we must 'clear' the bit by setting it to zero
     // while simultaneously preserving the other bits in the DDR register.
 
-    DDRB &= ~( 1 << DDB3);  // Clear the bit in DDRB3
-    PORTB |= (1 << PORTB3); // Enable pullup resistor by writing 1 to the port.
+    DDRB &= ~( 1 << DDD0);  // Clear the bit in DDRD0
+    PORTB |= (1 << PORTD0); // Enable pullup resistor by writing 1 to the port.
 
-    // Pin-Change Interrupts must be enabled for pin PB3
+    // Pin-Change Interrupts must be enabled for pin PD0
     PCICR |= (1 << PCIE0);   // Enable PCINT for 0-7
     PCMSK0 |= (1 << PCINT3); // Enable PCINT3 specifically in the pin change mask register 0
 }
