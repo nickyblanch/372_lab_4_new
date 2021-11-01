47ac2cbaa439efde7249f2de9a3f2369f16d91ed
nickyblanch <70228627+nickyblanch@users.noreply.github.com>
2021-11-01 14:26:27 -0700

    Changed pwm.cpp to correct math in changedutycycle

--- src/pwm.cpp
+++ src/pwm.cpp
@@ -1,44 +1,29 @@
 #include "pwm.h"
 
-// !!
-// TODO:
-// This code appears to be a ash-up of code for two different PWM modes: 10 bit fast, as well as the variable frequency mode.
-// Pretty sure we should only be changing the values in OCR4A and OCR3A (to control duty cycle), not OCR4C and OCR3C.
-// I'll look into it more tomorrow
-
 //copy pasted code from class
 void initPWMTimer4() {
+
     //set data direction pins
     DDRB |= (1<<PORTH3);
     DDRB |= (1<< PORTB7);
  
     //set for non inverting timer
-
     TCCR4A |= (1<<COM4A1);
     TCCR4A &= ~(1<<COM4A0);
-    
-
-
-
 
     //set the mode for the timer for  fast PWM 10 BIT
     TCCR4A |= (1<<WGM41);
     TCCR4A |= (1<<WGM40);
     TCCR4B |= (1<<WGM42);
     TCCR4B &= (1<< WGM43);
+
     //set prescaler to 1
     TCCR4B|= (1<<CS40);
     TCCR4B &= ~(1<<CS41);
     TCCR4B &= ~(1<<CS42);
 
-
-
-//initialize the output compare register to ****
-OCR4A =251;
-
-// set the duty cycle to 75%
-
-OCR4C=600;
+    //initialize the output compare register to ****
+    OCR4A =251;
 
 }
 
@@ -53,32 +38,20 @@ void initPWMTimer3() {
 
     TCCR3A |= (1<<COM3A0);
     TCCR3A &= ~(1<<COM3A1);
-    
-
-
-
 
     //set the mode for the timer for  fast PWM 10 BIT
     TCCR3A |= (1<<WGM31);
     TCCR3A |= (1<<WGM30);
     TCCR3B |= (1<<WGM32);
     TCCR3B &= (1<< WGM33);
+
     //set prescaler to 1
     TCCR3B|= (1<<CS30);
     TCCR3B &= ~(1<<CS31);
     TCCR3B &= ~(1<<CS32);
 
-
-
-//initialize the output compare register to ****
-OCR3A =251;
-
-
-
-}
-void changeDutyCycle(unsigned int adc){
-OCR3A=adc;
-OCR4A=adc;
+    //initialize the output compare register to ****
+    OCR3A =251;
 
 }
 
@@ -92,16 +65,19 @@ void changeDutyCycle(unsigned short int adc_num) {
     // FIRST SCENARIO: WE ARE GOING CLOCKWISE (USE PWM ON TIMER 3)
     if (adc_num < 512) {
         // 0 represents max ramp (100% duty cycle,) and 512 represents min rmp (0% duty cycle)
-        OCR3A |= (1 << (0b1111111111 - adc_num));
+        OCR3A = 2*(1 << (0b1111111111 - adc_num));
+        OCR4A = 0;
     }
     // SECOND SCENARIO: WE ARE GOING CLOCKWISE (USE PWM ON TIMER 3)
     else if (adc_num > 512) {
         // 0 represents max ramp (100% duty cycle,) and 512 represents min rmp (0% duty cycle)
-        OCR4A |= (1 << (adc_num));
+        OCR4A = 2*(1 << (adc_num));
+        OCR3A = 0;
     }
     // THIRD SCENARIO: WE ARE NOT MOVING THE MOTOR
     else {
-        OCR3A |= (0);
+        OCR3A = (0);
+        OCR4A = 0;
     }
 
 }
