98dee954b343f9ed7312a650bae3a4ba8f1b914a
nickyblanch <70228627+nickyblanch@users.noreply.github.com>
2021-11-08 16:26:12 -0700

    Update seve_segment_ec

---
+++ src/seven_segment_ec.cpp
@@ -0,0 +1,9 @@
+// Authors: Nick, Konner
+// Date: 11/8/21
+// Course: ECE 372 University of Arizona
+// Description: This file implements a shift register
+//              to drive a 7-segment display.
+
+#include <avr/io.h>
+#include "Arduino.h"
+
